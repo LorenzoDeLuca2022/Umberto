21012a0d4ee0f1abef6d63dd84d3ea9275828cc4
LorenzoDeLuca2022 <deluca.lorenzo@iisgalvanimi.edu.it>
2023-01-14 16:37:36 +0000

    es4

--- app.py
+++ app.py
@@ -22,12 +22,27 @@ import random
 lista4 = []
 for i in range (50):
     lista4.append(random.randrange(1,100))
-print(lista4)
+#print(lista4)
 lista5 = []
 for x in lista4:
     if (x % 2) != 0:
         lista5.append(x)
-print(lista5)
+#print(lista5)
 
 #°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸
+
+#Riempi una lista di 50 numeri casuali compresi tra 1 e 100 (utilizza un ciclo for) 
+#e stampa solo i numeri maggiori di 50 o minori di 10
+lista6 = []
+lista7 = []
+for i in range (50):
+    lista6.append(random.randrange(1,100))
+print(lista6)
+for x in lista6:
+    if x > 50 or x < 10:
+        lista7.append(x)
+print(lista7)
+print(len(lista7))
+        
+
 
