0d3e3a5a5dd2067ef822fd0086968a7cc757064f
LorenzoDeLuca2022 <deluca.lorenzo@iisgalvanimi.edu.it>
2023-01-14 15:51:31 +0000

    es2

--- app.py
+++ app.py
@@ -7,4 +7,14 @@ lista1 = [x,y,z]
 lista1.sort ()
 print(lista1)
 
-°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸
+#°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸
+
+#Riempi una lista con 5 nomi di automobili e una con 4 Moto, unisci le liste. Stampa la lista risultato in ordine alfabetico
+lista2 = ["Ferrari", "Lamborghini", "Jaguar", "Astor Martin", "McLaren"]
+lista3 = ["Ducati", "Yamaha", "Kawasaki", "Harley-Davidson"]
+lista = lista2 + lista3
+lista.sort ()
+print(lista)
+
+#°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸
+
