49b8017ad7b76d25f46daff87ccbb60408df1024
LorenzoDeLuca2022 <deluca.lorenzo@iisgalvanimi.edu.it>
2023-01-14 15:45:52 +0000

    es1

---
+++ app.py
@@ -0,0 +1,10 @@
+#Riempi 3 variabili con numeri casuali. Stampa i valori in ordine (dal più piccolo al più grande)
+import random
+x = (random.randrange(1,10))
+y = (random.randrange(1,10))
+z = (random.randrange(1,10))
+lista1 = [x,y,z]
+lista1.sort ()
+print(lista1)
+
+°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸
