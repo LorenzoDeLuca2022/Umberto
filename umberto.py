317385a5a33b77cddada6f5ebc68a2c04e170466
LorenzoDeLuca2022 <deluca.lorenzo@iisgalvanimi.edu.it>
2023-01-14 16:44:25 +0000

    es2

--- umberto.py
+++ umberto.py
@@ -3,3 +3,8 @@ import random
 z = random.randrange(10,20)
 print(z)
 
+#Riempi una lista (chiamata listone1) con z numeri casuali tra 0 e 20, dove z è la variabile creata al punto 1.
+lista1 = []
+for i in range (z):
+    lista1.append(random.randrange(0,20))
+print(lista1)
